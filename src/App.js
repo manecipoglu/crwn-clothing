d9bb804ae103060a09efd5c7a5aaf126851f9645
manecipoglu <muammernecipoglu@gmail.com>
2021-06-08 15:24:04 -0700

    setup navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import HomePage from "./components/Homepage";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
       </div>
     );
   }
