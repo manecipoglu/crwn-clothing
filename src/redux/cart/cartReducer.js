d942d0ac3330ee90d96b2867853729cff628d84b
manecipoglu <muammernecipoglu@gmail.com>
2021-06-11 18:58:13 -0700

    add a utility function to help group the same items in cart

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -11,13 +11,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden,
       };
     case "ADD_ITEM":
-      return {
-        ...state,
-        cartItems: [...state.cartItems, action.payload],
-      };
+      return { ...state, cartItems: addItem(state.cartItems, action.payload) };
     default:
       return state;
   }
 };
 
 export default cartReducer;
+
+export const addItem = (cartItems, itemToAdd) => {
+  const exists = cartItems.find(item => item.id === itemToAdd.id);
+
+  if (exists) {
+    return cartItems.map(item =>
+      item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+  }
+
+  return [...cartItems, { ...itemToAdd, quantity: 1 }];
+};
