86431bef6a160a9ea536ff264fab80d8c68f8757
manecipoglu <muammernecipoglu@gmail.com>
2021-06-11 17:44:21 -0700

    add addItem functionality in the CollectionItem component

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,20 +1,31 @@
 import "./CollectionItem.scss";
 import CustomButton from "./CustomButton";
+import { addItem } from "../redux/cart/cartActions";
+import { connect } from "react-redux";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to Cart
+      </CustomButton>
     </div>
-    <CustomButton inverted>Add to Cart</CustomButton>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatch = {
+  addItem,
+};
+
+export default connect(null, mapDispatch)(CollectionItem);
