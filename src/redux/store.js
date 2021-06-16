b60e025e6373278bdad88e656a0cf6a4a7a1c963
manecipoglu <muammernecipoglu@gmail.com>
2021-06-16 15:53:13 -0700

    add styled components for customButton component

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,12 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
-const middleware = [logger];
+const middlewares = [];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
