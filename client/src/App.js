1e5c4a3b8d1e6c258c1bd08e5687730d177c79a9
manecipoglu <muammernecipoglu@gmail.com>
2021-06-25 08:10:54 -0700

    utilize lazy loading of different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import { useEffect, useRef } from "react";
-import HomePage from "./pages/Homepage";
-import ShopPage from "./pages/ShopPage";
-import RegisterPage from "./pages/RegisterPage";
-import CheckoutPage from "./pages/CheckoutPage";
+import { useEffect, useRef, lazy, Suspense } from "react";
+
 import Header from "./components/Header";
+import Spinner from "./components/Spinner";
 import "./App.scss";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { auth, createUserProfileDocument } from "./firebase/utils";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./redux/user/userActions";
 import { selectCurrentUser } from "./redux/user/userSelectors";
+
+const HomePage = lazy(() => import("./pages/Homepage"));
+const ShopPage = lazy(() => import("./pages/ShopPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const CheckoutPage = lazy(() => import("./pages/CheckoutPage"));
 
 function App() {
   const currentUser = useSelector(selectCurrentUser);
@@ -43,15 +46,19 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop/*" element={<ShopPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route
-          path="/signin"
-          element={currentUser ? <Navigate to="/" replace /> : <RegisterPage />}
-        />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop/*" element={<ShopPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route
+            path="/signin"
+            element={
+              currentUser ? <Navigate to="/" replace /> : <RegisterPage />
+            }
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
